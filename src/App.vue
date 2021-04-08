0c72226812a3c152576d792934946e5491244471
rjmcbain <rmcbain01@gmail.com>
2021-04-08 14:19:56 -0700

    setting up components

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <StoredResources :resources="storedResources" />
+</template>
+
+<script>
+import StoredResources from "./components/StoredResources";
+export default {
+  name: "App",
+  components: { StoredResources },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: "official-guide",
+          title: "Official Guide",
+          description: "The official Vue.js documentation",
+          link: "https://vuejs.org",
+        },
+        {
+          id: "google",
+          title: "Google",
+          description: "Learn to google...",
+          link: "https://www.google.com",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Roboto", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/StoredResources.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul>
+    <LearningResource
+      v-for="res in resources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    />
+  </ul>
+</template>
+
+<script>
+import LearningResource from "../components/LearningResource";
+export default {
+  components: {
+    LearningResource,
+  },
+  props: ["resources"],
+};
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
